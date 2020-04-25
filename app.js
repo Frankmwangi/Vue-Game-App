b8b4c713a237715898c8304acb6794e32e7301fe
Frank Mwangi <frankmwangi007@gmail.com>
2020-04-25 22:08:48 +0300

    Working on attack button using conditons

--- app.js
+++ app.js
@@ -4,5 +4,61 @@ new Vue({
         playerHealth: 100,
         monsterHealth:100,
         gameIsRunning: false
+    },
+    methods: {
+        startGame:function(){
+            this.gameIsRunning=true;
+            this.playerHealth=100;
+            this.monsterHealth=100;
+        },
+        attack: function(){
+        this.monsterHealth -= this.calculateDamage(3,10);
+         if(this.checkWin()) {
+             return;
+         }  
+
+            
+        this.playerHealth -= this.calculateDamage(5,12);
+            if(this.playerHealth <= 0){
+                alert('You lost!');
+                this.gameIsRunning = false;
+            }
+            this.checkWin();
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveup: function(){
+
+        },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random()*max) +1,min)
+
+        },
+        checkWin: function(){
+            if(this.monsterHealth <= 0){
+                if(confirm('You won! New Game?')){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                    
+                }
+                return;
+            }else if(this.playerHealth <=0){
+                    if(confirm('You lost! New Game?')){
+                        this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return;
+
+            }
+            return false;
+        }
+            
+        
     }
-})
+});
